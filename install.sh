bb3f47caa8c3a187b485cded4665e9f5317ab680
CPressland <mail@cpressland.io>
2016-06-08 09:43:05 +0100

    Added Install Script

---
+++ install.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+yum install -y git
+mkdir -p /root/chef/cache
+mkdir -p /root/chef/cookbooks
+curl -L https://www.chef.io/chef/install.sh | bash -s -- -v 12.7.2
+git clone https://github.com/cpressland/cookbook-appserver.git /root/chef/cookbooks/appserver
+git clone https://github.com/chef-cookbooks/yum.git /root/chef/cookbooks/yum
+git clone https://github.com/rigrassm/firewalld-cookbook.git /root/chef/cookbooks/firewalld
+git clone https://github.com/skottler/selinux.git /root/chef/cookbooks/selinux
+
+
+echo '
+{
+  "run_list": [ "recipe[appserver::default]" ]
+}' > /root/chef/node.json
+
+
+echo '
+  file_cache_path "/root/chef/cache"
+  cookbook_path "/root/chef/cookbooks"
+  json_attribs "/root/chef/node.json"
+' > /root/chef/solo.rb
+
+sudo chef-solo -c /root/chef/solo.rb -j /root/chef/node.json
