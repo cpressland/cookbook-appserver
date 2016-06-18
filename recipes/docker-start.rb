6b95a393c197b20bceb1b644326a4b81f8c99db5
Chris Pressland <mail@cpressland.io>
2016-06-18 17:52:12 +0100

    added recipe

---
+++ recipes/docker-start.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: appserver
+# Recipe:: docker-start
+#
+# Copyright (C) 2016 Chris Pressland
+#
+# All rights reserved - Do Not Redistribute
+#
+
+pcontvars = node['docker']['permanentcontainers']
+
+pcontvars.each do |pcontainers|
+ docker_container pcontainers[:name] do
+   repo pcontainers[:repo]
+   tag pcontainers[:tag]
+   port pcontainers[:port]
+   network_mode pcontainers[:network_mode]
+   links pcontainers[:link]
+   volumes pcontainers[:volumes]
+   env pcontainers[:env]
+   restart_policy 'always'
+ end
+end
