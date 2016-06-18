1defb1af90c2672820bb4abdfacbd1355f28028e
Chris Pressland <mail@cpressland.io>
2016-06-18 21:33:28 +0100

    added docker-destroy recipe

---
+++ recipes/docker-destroy.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: appserver
+# Recipe:: docker-stop
+#
+# Copyright (C) 2016 Chris Pressland
+#
+# All rights reserved - Do Not Redistribute
+#
+
+pcontvars = node['docker']['permanentcontainers']
+
+pcontvars.each do |pcontainers|
+ docker_container pcontainers[:name] do
+   action [:stop, :delete]
+ end
+end
