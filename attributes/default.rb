84bbf18e650d486bd9f96a1bf9adaadcdbcba7cc
Chris Pressland <mail@cpressland.io>
2016-07-06 20:39:42 +0100

    updated attributes, added Plexpy container

--- attributes/default.rb
+++ attributes/default.rb
@@ -42,18 +42,19 @@ default['firewalld']['firewalld_services'] = [
 
 default['docker']['images'] = [
   { :name=>"ubuntu", :tag=>"xenial" },
-  { :name=>"cpressland/nginx", :tag=>"latest" },
-  { :name=>"cpressland/php", :tag=>"latest" },
+  { :name=>"cpressland/nginx", :tag=>"ubuntu" },
+  { :name=>"cpressland/php", :tag=>"ubuntu" },
   { :name=>"mariadb", :tag=>"latest" },
   { :name=>"ghost", :tag=>"latest" },
-  { :name=>"cpressland/nzbget", :tag=>"latest" },
-  { :name=>"cpressland/sonarr", :tag=>"latest" },
-  { :name=>"cpressland/couchpotato", :tag=>"latest" },
+  { :name=>"cpressland/nzbget", :tag=>"ubuntu" },
+  { :name=>"cpressland/sonarr", :tag=>"ubuntu" },
+  { :name=>"cpressland/couchpotato", :tag=>"ubuntu" },
   { :name=>"cpressland/plex", :tag=>"ubuntu" },
+  { :name=>"cpressland/plexpy", :tag=>"ubuntu" },
   { :name=>"cpressland/netdata", :tag=>"ubuntu" }
 ]
 
-default['docker']['volumes'] = %w(data_databases data_ghost data_www config_nginx config_netdata config_nzbget config_sonarr config_couchpotato config_plex)
+default['docker']['volumes'] = %w(data_databases data_ghost data_www config_nginx config_netdata config_nzbget config_sonarr config_couchpotato config_plex config_plexpy)
 
 default['docker']['restorecontainers'] = [
   { :name=>"restore-data_databases", :repo=>"ubuntu", :tag=>"xenial", :volumes=>['/media/shared/docker:/docker', 'data_databases:/data'], :command=>"/bin/tar xzf /docker/volumes/data_databases.tar.gz -C /data/ ." },
@@ -64,18 +65,20 @@ default['docker']['restorecontainers'] = [
   { :name=>"restore-config_sonarr", :repo=>"ubuntu", :tag=>"xenial", :volumes=>['/media/shared/docker:/docker', 'config_sonarr:/data'], :command=>"/bin/tar xzf /docker/volumes/config_sonarr.tar.gz -C /data/ ." },
   { :name=>"restore-config_couchpotato", :repo=>"ubuntu", :tag=>"xenial", :volumes=>['/media/shared/docker:/docker', 'config_couchpotato:/data'], :command=>"/bin/tar xzf /docker/volumes/config_couchpotato.tar.gz -C /data/ ." },
   { :name=>"restore-config_plex", :repo=>"ubuntu", :tag=>"xenial", :volumes=>['/media/shared/docker:/docker', 'config_plex:/data'], :command=>"/bin/tar xzf /docker/volumes/config_plex.tar.gz -C /data/ ." },
+  { :name=>"restore-config_plexpy", :repo=>"ubuntu", :tag=>"xenial", :volumes=>['/media/shared/docker:/docker', 'config_plexpy:/data'], :command=>"/bin/tar xzf /docker/volumes/config_plexpy.tar.gz -C /data/ ." },
   { :name=>"restore-config_netdata", :repo=>"ubuntu", :tag=>"xenial", :volumes=>['/media/shared/docker:/docker', 'config_netdata:/data'], :command=>"/bin/tar xzf /docker/volumes/config_netdata.tar.gz -C /data/ ." }
 ]
 
 default['docker']['permanentcontainers'] = [
   { :name=>"mariadb", :repo=>"mariadb", :tag=>"latest", :network_mode=>"cpressland.io", :volumes=>"data_databases:/var/lib/mysql" },
-  { :name=>"php", :repo=>"cpressland/php", :tag=>"latest", :network_mode=>"cpressland.io", :volumes=>"data_www:/var/www" },
+  { :name=>"php", :repo=>"cpressland/php", :tag=>"ubuntu", :network_mode=>"cpressland.io", :volumes=>"data_www:/var/www" },
   { :name=>"ghost", :repo=>"ghost", :tag=>"latest", :network_mode=>"cpressland.io", :volumes=>"data_ghost:/var/lib/ghost" },
-  { :name=>"nzbget", :repo=>"cpressland/nzbget", :tag=>"latest", :network_mode=>"cpressland.io", :volumes=>['config_nzbget:/config', '/downloads:/downloads'] },
-  { :name=>"sonarr", :repo=>"cpressland/sonarr", :tag=>"latest", :network_mode=>"cpressland.io", :volumes=>['config_sonarr:/config', '/downloads:/downloads', '/media/shared/px01/tv:/tv', '/dev/rtc:/dev/rtc:ro'] },
-  { :name=>"couchpotato", :repo=>"cpressland/couchpotato", :tag=>"latest", :network_mode=>"cpressland.io", :volumes=>['config_couchpotato:/config', '/downloads:/downloads', '/media/shared/px01/movies:/movies', '/etc/localtime:/etc/localtime:ro'] },
+  { :name=>"nzbget", :repo=>"cpressland/nzbget", :tag=>"ubuntu", :network_mode=>"cpressland.io", :volumes=>['config_nzbget:/config', '/downloads:/downloads'] },
+  { :name=>"sonarr", :repo=>"cpressland/sonarr", :tag=>"ubuntu", :network_mode=>"cpressland.io", :volumes=>['config_sonarr:/config', '/downloads:/downloads', '/media/shared/px01/tv:/tv', '/dev/rtc:/dev/rtc:ro'] },
+  { :name=>"couchpotato", :repo=>"cpressland/couchpotato", :tag=>"ubuntu", :network_mode=>"cpressland.io", :volumes=>['config_couchpotato:/config', '/downloads:/downloads', '/media/shared/px01/movies:/movies', '/etc/localtime:/etc/localtime:ro'] },
+  { :name=>"plexpy", :repo=>"cpressland/plexpy", :tag=>"ubuntu", :network_mode=>"cpressland.io", :volumes=>"config_plexpy:/config" },
   { :name=>"plex", :repo=>"cpressland/plex", :tag=>"ubuntu", :network_mode=>"host", :volumes=>['config_plex:/config', '/media/shared/px01/tv:/tv', '/media/shared/px01/movies:/movies'] },
-  { :name=>"nginx", :repo=>"cpressland/nginx", :tag=>"latest", :network_mode=>"cpressland.io", :port=>['80:80', '443:443'], :volumes=>['data_www:/var/www', 'config_nginx:/etc/nginx'] },
+  { :name=>"nginx", :repo=>"cpressland/nginx", :tag=>"ubuntu", :network_mode=>"cpressland.io", :port=>['80:80', '443:443'], :volumes=>['data_www:/var/www', 'config_nginx:/etc/nginx'] },
   { :name=>"netdata", :repo=>"cpressland/netdata", :tag=>"ubuntu", :network_mode=>"cpressland.io", :cap_add=>"SYS_PTRACE", :volumes=>['/proc:/host/proc:ro', '/sys:/host/sys:ro', '/var/run/docker.sock:/var/run/docker.sock', 'config_netdata:/etc/netdata/'] }
 ]
 
@@ -88,5 +91,6 @@ default['docker']['backupcontainers'] = [
   { :name=>"backup-config_sonarr", :repo=>"ubuntu", :tag=>"xenial", :volumes=>['/media/shared/docker:/docker', 'config_sonarr:/data'], :command=>"/bin/tar czf /docker/volumes/config_sonarr.tar.gz -C /data/ ." },
   { :name=>"backup-config_couchpotato", :repo=>"ubuntu", :tag=>"xenial", :volumes=>['/media/shared/docker:/docker', 'config_couchpotato:/data'], :command=>"/bin/tar czf /docker/volumes/config_couchpotato.tar.gz -C /data/ ." },
   { :name=>"backup-config_plex", :repo=>"ubuntu", :tag=>"xenial", :volumes=>['/media/shared/docker:/docker', 'config_plex:/data'], :command=>"/bin/tar czf /docker/volumes/config_plex.tar.gz -C /data/ ." },
+  { :name=>"backup-config_plexpy", :repo=>"ubuntu", :tag=>"xenial", :volumes=>['/media/shared/docker:/docker', 'config_plexpy:/data'], :command=>"/bin/tar czf /docker/volumes/config_plexpy.tar.gz -C /data/ ." },
   { :name=>"backup-config_netdata", :repo=>"ubuntu", :tag=>"xenial", :volumes=>['/media/shared/docker:/docker', 'config_netdata:/data'], :command=>"/bin/tar czf /docker/volumes/config_netdata.tar.gz -C /data/ ." }
 ]
