510c2034214eaa1726cba2c353b4ca3da8af5528
Chris Pressland <mail@cpressland.io>
2016-09-11 19:22:01 +0100

    updated attributes, removed nginx, php, mariadb, added caddy

--- attributes/default.rb
+++ attributes/default.rb
@@ -60,16 +60,13 @@ default['docker']['images'] = [
   { :name=>"cpressland/unifi", :tag=>"latest" }
 ]
 
-default['docker']['volumes'] = %w(data_databases data_ghost data_www config_nginx config_netdata config_nzbget config_sonarr config_couchpotato config_plex config_plexpy data_minecraft config_unifi config_nzbhydra data_influxdb)
+default['docker']['volumes'] = %w(data_ghost config_netdata config_nzbget config_sonarr config_couchpotato config_plex config_plexpy config_unifi config_nzbhydra config_caddy)
 
 default['docker']['restorecontainers'] = [
-  { :name=>"restore-data_databases", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'data_databases:/data'], :command=>"/bin/tar xzf /docker/volumes/data_databases.tar.gz -C /data/ ." },
   { :name=>"restore-data_ghost", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'data_ghost:/data'], :command=>"/bin/tar xzf /docker/volumes/data_ghost.tar.gz -C /data/ ." },
-  { :name=>"restore-data_www", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'data_www:/data'], :command=>"/bin/tar xzf /docker/volumes/data_www.tar.gz -C /data/ ." },
-  { :name=>"restore-data_minecraft", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'data_minecraft:/data'], :command=>"/bin/tar xzf /docker/volumes/data_minecraft.tar.gz -C /data/ ." },
-  { :name=>"restore-config_nginx", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_nginx:/data'], :command=>"/bin/tar xzf /docker/volumes/config_nginx.tar.gz -C /data/ ." },
+  { :name=>"restore-config_caddy", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_caddy:/data'], :command=>"/bin/tar xzf /docker/volumes/config_caddy.tar.gz -C /data/ ." },
   { :name=>"restore-config_nzbget", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_nzbget:/data'], :command=>"/bin/tar xzf /docker/volumes/config_nzbget.tar.gz -C /data/ ." },
-  { :name=>"restore-config_nzbget", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_nzbhydra:/data'], :command=>"/bin/tar xzf /docker/volumes/config_nzbhydra.tar.gz -C /data/ ." },
+  { :name=>"restore-config_nzbhydra", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_nzbhydra:/data'], :command=>"/bin/tar xzf /docker/volumes/config_nzbhydra.tar.gz -C /data/ ." },
   { :name=>"restore-config_sonarr", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_sonarr:/data'], :command=>"/bin/tar xzf /docker/volumes/config_sonarr.tar.gz -C /data/ ." },
   { :name=>"restore-config_couchpotato", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_couchpotato:/data'], :command=>"/bin/tar xzf /docker/volumes/config_couchpotato.tar.gz -C /data/ ." },
   { :name=>"restore-config_plex", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_plex:/data'], :command=>"/bin/tar xzf /docker/volumes/config_plex.tar.gz -C /data/ ." },
@@ -78,10 +75,20 @@ default['docker']['restorecontainers'] = [
   { :name=>"restore-config_unifi", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_unifi:/data'], :command=>"/bin/tar xzf /docker/volumes/config_unifi.tar.gz -C /data/ ." }
 ]
 
+default['docker']['backupcontainers'] = [
+  { :name=>"backup-data_ghost", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'data_ghost:/data'], :command=>"/bin/tar czf /docker/volumes/data_ghost.tar.gz -C /data/ ." },
+  { :name=>"backup-config_caddy", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_caddy:/data'], :command=>"/bin/tar czf /docker/volumes/config_caddy.tar.gz -C /data/ ." },
+  { :name=>"backup-config_nzbget", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_nzbget:/data'], :command=>"/bin/tar czf /docker/volumes/config_nzbget.tar.gz -C /data/ ." },
+  { :name=>"backup-config_nzbhydra", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_nzbhydra:/data'], :command=>"/bin/tar czf /docker/volumes/config_nzbhydra.tar.gz -C /data/ ." },
+  { :name=>"backup-config_sonarr", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_sonarr:/data'], :command=>"/bin/tar czf /docker/volumes/config_sonarr.tar.gz -C /data/ ." },
+  { :name=>"backup-config_couchpotato", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_couchpotato:/data'], :command=>"/bin/tar czf /docker/volumes/config_couchpotato.tar.gz -C /data/ ." },
+  { :name=>"backup-config_plex", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_plex:/data'], :command=>"/bin/tar czf /docker/volumes/config_plex.tar.gz -C /data/ ." },
+  { :name=>"backup-config_plexpy", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_plexpy:/data'], :command=>"/bin/tar czf /docker/volumes/config_plexpy.tar.gz -C /data/ ." },
+  { :name=>"backup-config_netdata", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_netdata:/data'], :command=>"/bin/tar czf /docker/volumes/config_netdata.tar.gz -C /data/ ." },
+  { :name=>"backup-config_unifi", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_unifi:/data'], :command=>"/bin/tar czf /docker/volumes/config_unifi.tar.gz -C /data/ ." }
+]
+
 default['docker']['permanentcontainers'] = [
-  { :name=>"influxdb", :repo=>"influxdb", :tag=>"0.13", :network_mode=>"cpressland.io", :port=>['8083:8083', '8086:8086'], :volumes=>"data_influxdb:/var/lib/influxdb" },
-  { :name=>"mariadb", :repo=>"mariadb", :tag=>"latest", :network_mode=>"cpressland.io", :volumes=>"data_databases:/var/lib/mysql" },
-  { :name=>"php", :repo=>"cpressland/php", :tag=>"latest", :network_mode=>"cpressland.io", :volumes=>"data_www:/var/www" },
   { :name=>"ghost", :repo=>"ghost", :tag=>"latest", :network_mode=>"cpressland.io", :volumes=>"data_ghost:/var/lib/ghost" },
   { :name=>"nzbget", :repo=>"cpressland/nzbget", :tag=>"latest", :network_mode=>"cpressland.io", :volumes=>['config_nzbget:/config', '/downloads:/downloads'] },
   { :name=>"nzbhydra", :repo=>"cpressland/nzbhydra", :tag=>"latest", :network_mode=>"cpressland.io", :volumes=>"config_nzbhydra:/config" },
@@ -89,27 +96,11 @@ default['docker']['permanentcontainers'] = [
   { :name=>"couchpotato", :repo=>"cpressland/couchpotato", :tag=>"latest", :network_mode=>"cpressland.io", :volumes=>['config_couchpotato:/config', '/downloads:/downloads', '/media/shared/px01/movies:/movies', '/etc/localtime:/etc/localtime:ro'] },
   { :name=>"plexpy", :repo=>"cpressland/plexpy", :tag=>"latest", :network_mode=>"cpressland.io", :volumes=>"config_plexpy:/config" },
   { :name=>"plex", :repo=>"cpressland/plex", :tag=>"latest", :network_mode=>"host", :volumes=>['config_plex:/config', '/media/shared/px01/tv:/tv', '/media/shared/px01/movies:/movies'] },
-  { :name=>"nginx", :repo=>"cpressland/nginx", :tag=>"latest", :network_mode=>"cpressland.io", :port=>['80:80', '443:443'], :volumes=>['data_www:/var/www', 'config_nginx:/etc/nginx'] },
+  { :name=>"caddy", :repo=>"cpressland/caddy", :tag=>"latest", :network_mode=>"cpressland.io", :port=>['80:80', '443:443'], :volumes=>"config_caddy:/config" },
   { :name=>"netdata", :repo=>"cpressland/netdata", :tag=>"latest", :network_mode=>"cpressland.io", :cap_add=>"SYS_PTRACE", :volumes=>['/proc:/host/proc:ro', '/sys:/host/sys:ro', '/var/run/docker.sock:/var/run/docker.sock', 'config_netdata:/etc/netdata/'] },
-  { :name=>"minecraft", :repo=>"cpressland/1710-pack", :tag=>"latest", :network_mode=>"cpressland.io", :port=>"25565:25565", :volumes=>"data_minecraft:/data" },
   { :name=>"unifi", :repo=>"cpressland/unifi", :tag=>"latest", :network_mode=>"cpressland.io", :port=>"8080:8080", :volumes=>"config_unifi://usr/lib/unifi/data" }
 ]
 
 default['docker']['transientcontainers'] = [
   { :name=>"backup-data_minecraft", :repo=>"cpressland/tools", :tag=>"latest", :volumes=>['/media/shared/docker:/docker', 'data_minecraft:/data'], :command=>"/bin/tar czf /docker/volumes/data_minecraft.tar.gz -C /data/ ." },
-]
-
-default['docker']['backupcontainers'] = [
-  { :name=>"backup-data_databases", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'data_databases:/data'], :command=>"/bin/tar czf /docker/volumes/data_databases.tar.gz -C /data/ ." },
-  { :name=>"backup-data_ghost", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'data_ghost:/data'], :command=>"/bin/tar czf /docker/volumes/data_ghost.tar.gz -C /data/ ." },
-  { :name=>"backup-data_www", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'data_www:/data'], :command=>"/bin/tar czf /docker/volumes/data_www.tar.gz -C /data/ ." },
-  { :name=>"backup-data_minecraft", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'data_minecraft:/data'], :command=>"/bin/tar czf /docker/volumes/data_minecraft.tar.gz -C /data/ ." },
-  { :name=>"backup-config_nginx", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_nginx:/data'], :command=>"/bin/tar czf /docker/volumes/config_nginx.tar.gz -C /data/ ." },
-  { :name=>"backup-config_nzbget", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_nzbget:/data'], :command=>"/bin/tar czf /docker/volumes/config_nzbget.tar.gz -C /data/ ." },
-  { :name=>"backup-config_sonarr", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_sonarr:/data'], :command=>"/bin/tar czf /docker/volumes/config_sonarr.tar.gz -C /data/ ." },
-  { :name=>"backup-config_couchpotato", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_couchpotato:/data'], :command=>"/bin/tar czf /docker/volumes/config_couchpotato.tar.gz -C /data/ ." },
-  { :name=>"backup-config_plex", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_plex:/data'], :command=>"/bin/tar czf /docker/volumes/config_plex.tar.gz -C /data/ ." },
-  { :name=>"backup-config_plexpy", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_plexpy:/data'], :command=>"/bin/tar czf /docker/volumes/config_plexpy.tar.gz -C /data/ ." },
-  { :name=>"backup-config_netdata", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_netdata:/data'], :command=>"/bin/tar czf /docker/volumes/config_netdata.tar.gz -C /data/ ." },
-  { :name=>"backup-config_unifi", :repo=>"alpine", :tag=>"3.4", :volumes=>['/media/shared/docker:/docker', 'config_unifi:/data'], :command=>"/bin/tar czf /docker/volumes/config_unifi.tar.gz -C /data/ ." }
 ]
